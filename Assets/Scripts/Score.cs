3d4370821c14b10d7c1009e9f606a0e8b429e4cc
Suriya Kenma <60160019@go.buu.ac.th>
2020-04-29 18:09:42 +0700

    updated highscore system v0.8.1

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -7,9 +7,14 @@ public class Score : MonoBehaviour
 {
     public Transform player;
     public Text scoreText;
+    public Text highscoreText;
     public float time = 0;
     public int score = 0;
+    private void Start()
+    {
+        highscoreText.text = "BEST : " + PlayerPrefs.GetInt("Highscore").ToString();
 
+    }
     // Update is called once per frame
     void Update()
     {
@@ -21,6 +26,11 @@ public class Score : MonoBehaviour
                 if (!PlayerCollision.isPlayerHit)
                 {
                     score += 1;
+                    if (score > PlayerPrefs.GetInt("Highscore"))
+                    {
+                        PlayerPrefs.SetInt("Highscore", score);
+                        highscoreText.text = "BEST : " + PlayerPrefs.GetInt("Highscore").ToString();
+                    }
                 }
                 time = 0;
             }
